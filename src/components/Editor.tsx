26187f0d67d6880b213a9618c11bb7a0e5a1059c
Amirhossein Mazaheri <uplayggboy@gmail.com>
2023-06-09 11:03:41 +0330

    feat: added automatic height to Editor textarea and a name prop

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, useRef } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 
 interface EditorProps {
   title: string;
+  name: string;
   value: string;
   onChange: (val: string) => void;
   highlighter?: (val: string) => string;
@@ -11,12 +12,22 @@ interface EditorProps {
 const Editor: React.FC<EditorProps> = ({
   title,
   value,
+  name,
   onChange,
   highlighter,
   className,
 }) => {
+  const [height, setHeight] = useState(0);
+
+  const containerRef = useRef<HTMLDivElement>(null);
   const preRef = useRef<HTMLPreElement>(null);
   const textAreaRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      setHeight(containerRef.current.clientHeight);
+    }
+  }, []);
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     onChange(e.target.value);
@@ -33,26 +44,30 @@ const Editor: React.FC<EditorProps> = ({
 
   return (
     <div
+      ref={containerRef}
       className={`flex flex-col overflow-hidden rounded-xl text-sm ${className}`}
     >
       <h3 className="w-full bg-red-500 px-4 py-[0.8rem] text-sm font-semibold">
         {title}
       </h3>
 
-      <div className="relative">
+      <div className="relative py-24">
         <textarea
           ref={textAreaRef}
-          className={`absolute left-0 top-0 z-[1] h-60 w-full resize-none overflow-auto whitespace-nowrap rounded-b-xl bg-transparent px-6 py-4 font-light leading-loose text-transparent caret-white outline-none `}
+          name={name}
+          className={`absolute left-0 top-0 z-[1] w-full resize-none overflow-auto whitespace-nowrap rounded-b-xl bg-transparent px-6 py-4 font-light leading-loose text-transparent caret-white outline-none `}
           onInput={handleChange}
           onScroll={handleScroll}
           spellCheck={false}
           value={value}
+          style={{ height }}
         ></textarea>
 
         <pre
           aria-hidden
           ref={preRef}
-          className="absolute top-0 z-0 h-60 w-full overflow-auto whitespace-nowrap rounded-b-xl bg-gray-700/50 px-6 py-4 font-light leading-loose"
+          style={{ height }}
+          className="absolute top-0 z-0 w-full overflow-auto whitespace-nowrap rounded-b-xl bg-gray-700/50 px-6 py-4 font-light leading-loose"
         >
           <code
             className="leading-loose"
