5989aa6795a83d26bfc76fe60e84e454d22962c5
Amirhossein Mazaheri <uplayggboy@gmail.com>
2023-06-16 19:08:54 +0330

    remove: removed getBA

--- src/utils/getBA.ts
+++
@@ -1,6 +0,0 @@
-import baseAssembly from "../../base_assembly.txt?raw";
-import { escapeRegExp } from "./getBMPM";
-
-export function getBA() {
-  return baseAssembly.replace(new RegExp(escapeRegExp("\r"), "g"), "");
-}
